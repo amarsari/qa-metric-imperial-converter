c9ea601352afb3b10e925ad34502da67a0640373
Antonio Marsari <antonio.marsari@gmail.com>
2025-03-25 10:35:50 -0300

    Convert Handler logic

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,26 +1,102 @@
+function numberStringSplitter(input) {
+  //this is a helper function that takes a string input and returns an array with the number and the string separated
+  let number = input.match(/[.\d\/]+/g) || ['1'];
+  let string = input.match(/[a-zA-Z]+/g)[0];
+
+  return [number[0], string];
+)
+
+function checkDiv(possibleFraction){
+  //this is a helper function
+  let nums = possibleFraction.split('/');
+  if (nums.length > 2){
+    return false;
+  }
+  return nums;
+}
+
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    let result;
-    
+    let result = numberStringSplitter(input)[0];
+    let nums = checkDiv(result);
+    if (!nums){
+      return undefined;
+    }
+
+    let num1 = nums[0];
+    let num2 = nums[1] || "1";
+    result = parseFloat(num1) / parseFloat(num2);
+    if (isNaN(num1) || isNaN(num2)){
+      return undefined;
+    }
     return result;
   };
   
   this.getUnit = function(input) {
-    let result;
+    let result = numberStringSplitter(input)[1].toLowerCase();
+    switch(result){
+      case "gal":
+        return "gal";
+      case "l":
+        return "L";
+      case "lbs":
+        return "lbs";
+      case "kg":
+        return "kg";
+      case "mi":
+        return "mi";
+      case "km":
+        return "km";
+      default:
+        return undefined;
+    }
     
     return result;
   };
   
   this.getReturnUnit = function(initUnit) {
-    let result;
+    let result = initUnit.toLowerCase();
     
+    switch(result){
+      case "gal":
+        return "L";
+      case "l":
+        return "gal";
+      case "lbs":
+        return "kg";
+      case "kg":
+        return "lbs";
+      case "mi":
+        return "km";
+      case "km":
+        return "mi";
+      default:
+        return undefined;
+    }
+
     return result;
   };
 
   this.spellOutUnit = function(unit) {
-    let result;
+    let result = unit.toLowerCase();
     
+    switch(result){
+      case "gal":
+        return "gallons";
+      case "l":
+        return "liters";
+      case "lbs":
+        return "pounds";
+      case "kg":
+        return "kilograms";
+      case "mi":
+        return "miles";
+      case "km":
+        return "kilometers";
+      default:
+        return undefined;
+    }
     return result;
   };
   
@@ -28,15 +104,37 @@ function ConvertHandler() {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    let result;
     
-    return result;
+    let unit = initUnit.toLowerCase();
+    let result;
+
+    switch (unit){
+      case "gal":
+        result = initNum * galToL;
+        break;
+      case "l":
+        result = initNum / galToL;
+        break;
+      case "lbs":
+        result = initNum * lbsToKg;
+        break;
+      case "kg":
+        result = initNum / lbsToKg;
+        break;
+      case "mi":
+        result = initNum * miToKm;
+        break;
+      case "km":
+        result = initNum / miToKm;
+        break;
+      default:
+        result = undefined;
+    }
+    return parseFloat(result.toFixed(5));
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
+    return `${initNum} ${this.spellOutUnit(initUnit)} converts to ${returnNum} ${this.spellOutUnit(returnUnit)}`;
   };
   
 }
